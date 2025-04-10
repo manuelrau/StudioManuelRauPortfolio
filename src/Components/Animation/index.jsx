6714d2243dd878bf9f55e7c5c12f24f17dd955c9
manuelrau <manuelrau@icloud.com>
2025-04-10 16:30:30 +0200

    install motion plugin

---
+++ src/Components/Animation/index.jsx
@@ -0,0 +1,21 @@
+import {useEffect, useRef} from "react";
+
+
+const Animation = () => {
+    const controls = useAnimation();
+    const ref = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entries]) => {
+
+        })
+
+    }, []);
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Animation
---
+++ src/Components/Animation/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+import { motion, useAnimatio } from "framer-motion";
+
+export const AnimationBox = styled(motion.div)`
+`
