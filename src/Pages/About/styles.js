362d084ec8217e04e352cdaa4b28483cd60050b6
manuelrau <manuelrau@icloud.com>
2025-05-27 15:42:38 +0200

    edit about styling

--- src/Pages/About/styles.js
+++ src/Pages/About/styles.js
@@ -31,36 +31,48 @@ export const AboutPtag = styled.p.attrs(() => ({
 `
 
 export const AboutText = styled.p.attrs(() => ({
-    className: 'text-sm'
-}))` 
+    className: 'text-base'
+}))`
+    text-indent: 15px;
 `
 export const Section = styled.section`
-    display: flex;
+    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    flex-shrink: 0;
+    grid-gap: 1rem;
     margin: 100px 0 100px 0;
 
     @media (max-width: 1024px) {
-
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 0.5rem;
     }
     
     @media (max-width: 768px) {
-        flex-direction: column;
+        grid-template-columns: repeat(2, 1fr);
     }
     
     @media (max-width: 480px) {
-        flex-direction: column;
+        grid-template-columns: repeat(2, 1fr);
     }
 `
 export const Container = styled.div`
     display: flex;
+    justify-content: space-between;
     flex-direction: column;
-    justify-content: flex-start;
     align-items: normal;
+    margin-bottom: 5rem;
     padding: 0 20px 0 20px;
 `
-export const Images = styled.img` 
-    height: 40vh;
-    max-width: 80%;
-    object-fit: contain;
+export const Images = styled.img`
+    max-width: 100%;
+    height: 100%;
+    margin: 0;
+    display: block;
+    break-inside: avoid;
+    transition: transform 0.3s ease;
+    border-radius: 5px;
+    object-fit: cover;
 `
 
 export const Wrapper = styled.div`
@@ -77,5 +89,8 @@ export const HeadlineH1 = styled.h1.attrs(() => ({
 
 export const HeadlineH3 = styled.h3.attrs(() => ({
     className: 'headline-h3'
+
+
 }))`
+    margin:  0 0 30px 0;
 `
